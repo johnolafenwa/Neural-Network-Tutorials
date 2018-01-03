2432cf00aff472b0c62de9c3fdc8769ca9af5a54
johnolafenwa <35049820+johnolafenwa@users.noreply.github.com>
2018-01-03 03:29:10 +0100

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,35 @@
+import keras
+from keras.datasets import mnist
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD
+
+
+(train_x, train_y) , (test_x, test_y) = mnist.load_data()
+
+print(train_x.shape)
+print(train_y.shape)
+print(test_x.shape)
+print(test_y.shape)
+
+train_x = train_x.reshape(60000,784)
+test_x = test_x.reshape(10000,784)
+
+train_y = keras.utils.to_categorical(train_y,10)
+test_y = keras.utils.to_categorical(test_y,10)
+
+model = Sequential()
+model.add(Dense(units=128,activation="relu",input_shape=(784,)))
+model.add(Dense(units=128,activation="relu"))
+model.add(Dense(units=128,activation="relu"))
+model.add(Dense(units=10,activation="softmax"))
+
+model.compile(optimizer=SGD(0.001),loss="categorical_crossentropy",metrics=["accuracy"])
+model.fit(train_x,train_y,batch_size=32,epochs=10,verbose=1)
+
+accuracy = model.evaluate(x=test_x,y=test_y,batch_size=32)
+
+print("Accuracy: ",accuracy)
+
+
+
